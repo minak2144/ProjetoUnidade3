9d00cb715bd775a9d4b0772cf32845a8b90f9a27
minak2144 <52211059+minak2144@users.noreply.github.com>
2019-06-25 12:27:59 -0300

    Create funcoes.h

---
+++ funcoes.h
@@ -0,0 +1,21 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+void imprimirMenu();
+void tamanhoMatrizA();
+void tamanhoMatrizB();
+void preencherMatrizManualA();
+void preencherMatrizManualB();
+void preencherMatrizAleatorioA();
+void preencherMatrizAleatorioB();
+void imprimirMatrizA();
+void imprimirMatrizB();
+void imprimirMatrizC();
+void somarMatrizAB();
+void subtrairMatrizAB();
+void multiplicarMatrizAB();
+void lerArquivoMatrizA();
+void lerArquivoMatrizB();
+void escreverArquivoMatrizC();
+
+#endif // FUNCOES_H
