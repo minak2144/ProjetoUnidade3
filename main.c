6cba26c8c0a416bdb4289841fd09849c064c395e
minak2144 <52211059+minak2144@users.noreply.github.com>
2019-06-25 12:27:19 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+#include <funcoes.h>
+
+
+//Loop infinito para rodar a funcao imprimirMenu()
+int main(){
+  int loop = 0;
+
+  while(loop == 0){
+      imprimirMenu();
+  
+}
